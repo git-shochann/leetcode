681c679f49642c858247384048e8088af826cbf2
git-shochann <sho_825.sa216@docomo.ne.jp>
2023-03-06 14:19:44 +0900

    fix can not split dot

--- coding-test/m3/solution.go
+++ coding-test/m3/solution.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"unicode"
 )
 
 var count int
@@ -11,12 +12,14 @@ var count int
 // アルファベット多文字 or 数字から始まる単語が何種類あるか
 func Solution(str string) int {
 
-	slice := strings.Split(str, " ")
+	slice := strings.FieldsFunc(str, func(r rune) bool {
+		return unicode.IsSpace(r) || r == '.'
+	})
 	fmt.Printf("slice: %v\n", slice)
 	uniqueSlice := DeleteDuplicate(slice) // 重複の削除
 	fmt.Printf("uniqueSlice: %v\n", uniqueSlice)
 	for _, v := range uniqueSlice {
-		// [M3 2000 sho tsuboya]
+		// Favorite food is ramen. Age 25 years old. hobby coding
 		initial := v[0:1]
 		ok := CheckRegex(initial)
 		if ok {
@@ -26,7 +29,7 @@ func Solution(str string) int {
 	return count
 }
 
-// [M3 2000 sho tsuboya M3]
+// [Favorite food is ramen. Age is 25 years old. Favorite hobby is coding]
 func DeleteDuplicate(strings []string) []string {
 	// m[""]false が初期化した際の型
 	m := make(map[string]bool)
--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"coding-test/coding-test/m3"
+	"fmt"
+	"os"
 )
 
 func main() {
 	// fmt.Println(valid_anagram.ValidAnagram("dog", "god")) // trueを返す
 	// fmt.Println(valid_anagram.ValidAnagram("cat", "god")) // falseを返す
-	// scanner := bufio.NewScanner(os.Stdin)
-	// scanner.Scan()
-	// text := scanner.Text()
-	// fmt.Printf("text: %v\n", text)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
+	fmt.Printf("text: %v\n", text)
 
-	// fmt.Printf("solution: %v\n", m3.Solution(text))
+	fmt.Printf("solution: %v\n", m3.Solution(text))
 
-	m3.OtherSolution()
+	//m3.OtherSolution()
 }
